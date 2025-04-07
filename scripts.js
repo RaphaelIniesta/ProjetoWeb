3f6a2eb44164625d4d2a94496559d3e24902d652
RaphaelIniesta <raphael_iniesta@yahoo.com>
2025-04-07 09:36:23 -0300

    Entrega final do Projeto Pichu

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
         accordion.addEventListener("click", function () {
             const panel = this.nextElementSibling;
             const seta = this.querySelector(".seta");
+
             if (panel.style.display === "block") {
                 panel.style.display = "none";
                 seta.style.transform = "rotate(0deg)";
@@ -15,4 +16,64 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
+
+    const form = document.getElementById("formMatricula");
+    const mensagem = document.getElementById("mensagemConfirmacao");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        mensagem.style.display = "block";
+        setTimeout(() => {
+            mensagem.style.display = "none";
+            form.reset();
+        }, 3000);
+    });
 });
+
+
+var code = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+
+var pos = 0;
+let set = false;
+
+document.addEventListener('keydown', function(e) {
+        
+    var key = e.key;
+    console.log(key);
+
+    if (key == code[pos]) {
+        pos++;
+        if (pos == code.length) {
+        rotate_page()
+        pos = 0;
+        }
+    } else {
+        pos = 0;
+    }
+});
+
+let degree = 0
+
+function rotate_page() {
+    if(document.body.style.rotate != "360deg") {
+        let x = setInterval(function() {
+            degree += 15;
+            document.body.style.transitionDuration = "0.5s"
+            document.body.style.rotate = degree + "deg";
+
+            if(degree == 360) {
+                clearInterval(x);
+            }
+        }, 10);
+    } else {
+        let x = setInterval(function() {
+            degree -= 15;
+            document.body.style.transitionDuration = "0.5s"
+            document.body.style.rotate = degree + "deg";
+
+            if(degree == 0) {
+                clearInterval(x);
+            }
+        }, 10);
+    }
+}
